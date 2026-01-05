6bd729a6ebaf358c27204cad236832b000d77e15
Hreshikesh <ailhreshikesh@gmail.com>
2026-01-05 15:05:58 +0530

    Food Delivery App with css

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,7 +5,7 @@ const Body = () => {
     return (
         <div>
             <h1 style={{ marginLeft: 180, fontSize: 50 }}>Biryani</h1>
-            <p style={{ marginLeft: 180, fontSize: 20, paddingBottom: 10 }}>Taste these delectable classics, delectable biryanis to make your day.
+            <p style={{ marginLeft: 180, fontSize: 20, marginBottom: 30 }}>Delicious Biryani restaurants near you
             </p>
             <div className="resto-card">
                 {
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,13 @@ const Header = () => {
     return (
         <div className="header">
             <div className="logo">
-                <img src="https://tse4.mm.bing.net/th/id/OIP.xFsfS9DLR3uSusNqNiKBBQHaGQ?pid=Api&P=0&h=180" style={{ width: 90 }} />
+                <img src="https://static.vecteezy.com/system/resources/previews/053/762/082/non_2x/food-delivery-catering-fast-food-logo-icon-free-vector.jpg" alt="Logo" width={70} />
             </div>
             <div>
                 <ul>
                     <li>Home</li>
                     <li>About</li>
-                    <li>contact</li>
+                    <li>Contact</li>
                 </ul>
             </div>
         </div>
